886e84c647e09f72146466617595e8679ce83aa3
Komal Joshi <155945813+komaljoshi28@users.noreply.github.com>
2025-12-26 15:56:34 +0530

    day3 codes

--- src/day3/CommandLineGradebook.java
+++ src/day3/CommandLineGradebook.java
@@ -5,6 +5,7 @@ Requirements
 
 Read n (number of students), then read n names and n integer grades (0–100).
 Store names in String[], grades in int[].
+
 Provide menu:
 
 Print all
@@ -49,12 +50,97 @@ Add Javadoc for each method.
 
 */ 
 package day3;
-
+import java.util.Scanner;
 public class CommandLineGradebook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of students: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		String[] names=new String[n];
+		int[] grades = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			System.out.print("Enter name of student " + (i+1) + ": ");
+            names[i] = sc.nextLine();
 
+            System.out.print("Enter grade of student " + (i+1) + ": ");
+            grades[i] = sc.nextInt();
+            sc.nextLine();
+		}
+		int menu_input = 0;
+		
+		while(true) {
+			System.out.println();
+			System.out.println("Menu options ");
+			System.out.println("1. Print all details ");
+			System.out.println("2. Print class topper ");
+			System.out.println("3. Print class average");
+			System.out.println("4. Search student by name ");
+			System.out.println("5. Curve grades by +5 ");
+			System.out.println("6. Exit ");
+			System.out.println("Enter digit to choose the option");
+			menu_input = sc.nextInt();
+			
+			switch(menu_input) {
+				case 1:
+					printDetails(names, grades);
+					break;
+				case 2:
+					classTopper(names, grades);
+					break;
+				case 3:
+					classAverage(grades);
+					break;
+				case 4:
+//					searchStudent(names, grades, sc);
+					break;
+				case 5:
+//					curveGrades(grades);
+					break;
+				case 6:
+					System.out.println("Exiting.. ");
+					sc.close();
+					return;
+				default:
+					System.out.println("Invalid choice");
+					continue;
+			}	
+		}
 	}
-
+	
+	public static void printDetails(String[] names, int[] grades) {
+		for(int i = 0;i< names.length ;i++) {
+			System.out.println("\nName: "+names[i] +" " + "Grades: "+grades[i]);
+		}
+	}
+	
+	public static void classTopper(String[] names, int[] grades) {
+		String toppername=names[0];
+		int maxgrade = grades[0];
+		for(int i =1;i<grades.length;i++) {
+			if(grades[i]> maxgrade) {
+				maxgrade=grades[i];
+				toppername = names[i];
+			}
+		}
+		System.out.println("Class topper is: " +toppername + " with grade: "+ maxgrade);
+	}
+	
+	public static void classAverage(int[] grades) {
+		int summ = grades[0];
+		for(int i =1;i<grades.length;i++) {
+			summ += grades[i];
+		}
+		
+		double average = (double)summ / grades.length;
+		System.out.printf("Average is: %.2f%n",average);
+		
+	}
+	
 }
+
--- src/day3/RobustStringAnalytics.java
+++ src/day3/RobustStringAnalytics.java
@@ -1,11 +1,11 @@
 /*
  * Robust String Analytics
 Objective: Analyze a line of text.
+
 Requirements
-
 Read a line of text.
-Compute:
 
+Compute:
 Number of words (split by whitespace).
 Number of sentences (. ! ?).
 Longest word.
@@ -23,9 +23,9 @@ Hello, world! Java 17 is amazing.
 Sample Output (partial)
 Words: 5
 Sentences: 2
-Digits: 2, Letters: 21
+Digits: 2, Letters: 23
 Longest word: "amazing"
-Vowel freq: a=4 e=1 i=1 o=2 u=0
+Vowel freq: a=4 e=1 i=2 o=2 u=0
 
 Constraints
 
@@ -38,10 +38,113 @@ Clean punctuation when finding longest word.
 
 package day3;
 
+import java.util.Scanner;
 public class RobustStringAnalytics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sentence here: ");
+		String input = sc.nextLine();
+		
+		int counter_Sentences = 0;
+		int counter_Digits =0;
+		int counter_Letters = 0;
+		int counter_Words = 0;
+		
+		
+		
+		// To get the longest word 
+		String[] words = input.split("\\s+"); //Treat multiple spaces as one separator //google 
+		String longestWord = "";
+		int maxLength = 0;
+		
+		for(int i = 0;i<words.length;i++) {
+			String currentWord = "";
+			int letter_Count = 0;
+			for(int j =0 ; j<words[i].length();j++) {
+				char ch = words[i].charAt(j); //individual characters in words 
+				
+				if(Character.isLetter(ch)) {
+					currentWord = currentWord + ch; //append character to word 
+					letter_Count += 1;
+				}
+			}
+			
+			// count valid words (must contain at least one letter)
+			if (letter_Count > 0) {
+				counter_Words++;
+			}
+		
+			if(letter_Count > maxLength) {
+				longestWord = currentWord;
+				maxLength = letter_Count;
+			}
+			
+		}
+		
+		
+
+	// to count the number of sentences 
+		for(int i = 0;i<input.length();i++) {
+			if (input.charAt(i) == '.' || input.charAt(i) == '?' || input.charAt(i) == '!') {
+				counter_Sentences += 1;
+			}
+		}
+		
+		//count number of digits 
+		for(int i = 0;i<input.length();i++) {
+			if(Character.isDigit(input.charAt(i))) {
+				counter_Digits += 1;
+			}
+		}
+		
+		//count number of letter 
+		for(int i = 0;i<input.length();i++) {
+			if(Character.isLetter(input.charAt(i))) {
+				counter_Letters +=1 ;
+			}
+		}
+		
+		//for vowel frequency 
+		String abc = input.toLowerCase();	
+		int[] vowelCounter = new int[5];
+		for(int i =0;i<abc.length();i++) {
+			char x = abc.charAt(i);
+			switch(x) {
+				case 'a':
+					vowelCounter[0]++;
+					break;
+				case 'e':
+					vowelCounter[1]++;
+					break;
+				case 'i':
+					vowelCounter[2]++;
+					break;
+				case 'o':
+					vowelCounter[3]++;
+					break;
+				case 'u':
+					vowelCounter[4]++;
+					break;
+				default:		
+			}
+		}
+		//print frequency of vowels 
+		char[] vowels = {'a','e','i','o','u'};
+		System.out.println("Vowel frequency: ");
+		for(int i =0;i<vowelCounter.length;i++) {
+			System.out.println(vowels[i] +" = " +vowelCounter[i]);
+		}
+		
+		System.out.println("Words: " +counter_Words);
+		System.out.println("Number of sentences: "+counter_Sentences);
+		System.out.println("Digits: " + counter_Digits + " Letters: "+ counter_Letters);
+		System.out.println("Longest word: " +longestWord);
+		
+		
+		sc.close();
+		
 
 	}
 
